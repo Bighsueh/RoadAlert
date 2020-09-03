d1a796699184985be48669c92e8850ab1002e350
Hsueh <40841121@gm.nfu.edu.tw>
2020-09-04 02:19:20 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/nfu/hsueh/roadalert/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package tw.edu.nfu.hsueh.roadalert.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
